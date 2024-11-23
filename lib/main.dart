04acf6b8193698bb95cec7ddcedfdc3b2f1d6ca9
oscar ayala ruelas <thezxzx2>
2024-11-23 15:14:31 -0700

    Tamaño mínimo de la ventana

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:tienda/ui/pages/pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1100, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+    minimumSize: Size(1100, 600),
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -24,7 +43,9 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [Locale("es")],
+      supportedLocales: const [
+        Locale("es"),
+      ],
       locale: const Locale("es"),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
